f457e242f0df08cb60b973d0a1a2e1207ff5ed72
Ohto Rainio <orainio@gmail.com>
2012-05-30 18:34:26 +0300

    First version of results page

---
+++ protected/models/VeikkausRivi.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * This is the model class storing the bets for one better.
+ * 
+ */
+class VeikkausRivi {
+	
+	var $veikkaaja;
+	var $lohkoVeikkaus = array();
+	var $voittajaVeikkaus = array();
+}
+
